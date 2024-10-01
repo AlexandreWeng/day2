9a76bba49e5a5187d985d85cfd26b30e51633398
AlexandreWeng <alexandre.weng@epitech.digital>
2024-10-01 16:14:10 +0200

    Update ex_07.js

--- ex_07.js
+++ ex_07.js
@@ -2,11 +2,9 @@ fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
 
 
 function whoisthatpokemon(poke) {
-    // Check if the index is -1, return the last fruit
     if (poke === -1) {
         return fruits[fruits.length - 1];
     }
-    
     if (poke >= 0 && poke < fruits.length) {
         return fruits[poke];
     }
