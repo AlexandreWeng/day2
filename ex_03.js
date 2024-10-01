a5246831b4cdc278cc2a6367e4152dac1d5c0999
AlexandreWeng <alexandre.weng@epitech.digital>
2024-10-01 10:34:27 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,17 @@
+function changebackground() {
+  var magicNumber1 = 1;
+  let magicNumber2 = 2;
+  let magicNumber3 = 3;
+
+  if (magicNumber1 === 1) {
+    paintYellowRectangle();
+  }
+  if (magicNumber2 === 2) {
+    paintGreenRectangle();
+  }
+  if (magicNumber3 === 3) {
+    paintRedRectangle();
+  }
+}
+
+changebackground();
